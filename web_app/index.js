9eb8a51434f448ebc9de024d6cd0a7a00f0e97b9
Gulyutin <gulyutin@gmail.com>
2023-08-10 16:03:46 +0700

    Updated front-end

--- web_app/index.js
+++ web_app/index.js
@@ -19,7 +19,7 @@ $(() => {
 
     async function getData(formData) {
         try {
-            const response = await axios.post("https://c3de-193-218-138-88.ngrok-free.app/api/v1/recognize", formData, {
+            const response = await axios.post("https://taiga.enisey.tech", formData, {
                 headers: {
                     "Content-Type": "multipart/form-data",
                 },
